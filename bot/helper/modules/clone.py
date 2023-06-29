ae473dd09eb83527dca0b58a2a3e4f6a744210d3
sahilgit55 <sahilgit55@gmail.com>
2023-06-29 16:32:23 +0530

    clone

--- bot/helper/modules/clone.py
+++ bot/helper/modules/clone.py
@@ -2,8 +2,10 @@ from pyrogram.handlers import MessageHandler
 from pyrogram.filters import command
 from re import search as re_search, findall as refindall
 from urllib.parse import parse_qs, urlparse
-from asyncio import create_subprocess_exec, create_subprocess_shell, sleep
+from asyncio import create_subprocess_exec, sleep
 from asyncio.subprocess import PIPE
+from string import ascii_lowercase, digits
+from random import choices
 
 from bot import config_dict, LOGGER, bot, BotCommands, bot_loop
 from bot.helper.pyrogram.message_utils import sendMessage, editMessage
@@ -11,6 +13,7 @@ from bot.helper.utils.other_utils import sync_to_async, is_share_link, is_gdrive
 from bot.helper.other.direct_link_generator import direct_link_generator
 from bot.helper.other.exceptions import DirectDownloadLinkException
 
+processes = set()
 
 def getIdFromUrl(link):
         if "folders" in link or "file" in link:
@@ -25,12 +28,13 @@ def getIdFromUrl(link):
 
 class CloneHelper:
     
-    def __init__(self, cmd):
-        self.cmd = cmd
+    def __init__(self, proc, pro_id):
         self.currentmsg = ''
         self.log = ''
         self.returncode = None
         self.complete = False
+        self.proc = proc
+        self.pro_id = pro_id
     
     def set_msg(self, msg):
         self.log += msg + "\n"
@@ -43,7 +47,8 @@ class CloneHelper:
                 eta = progress[3].strip().replace('ETA', '').strip()
                 text =f'Copying: {get_progress_bar_from_percentage(percentage)} {percentage}%\n'\
                         f'Copied: {dwdata[0].strip()} of {dwdata[1].strip()}\n'\
-                        f'Speed: {progress[2]} | ETA: {eta}'
+                        f'Speed: {progress[2]} | ETA: {eta}\n'\
+                        f'Cancel: <code>/clone cancel {self.pro_id}</code>'
                 self.currentmsg = text
         return
     
@@ -55,16 +60,13 @@ class CloneHelper:
         self.complete = True
         return
 
-@new_task
-async def cmd_exec_status(cloneob, shell=False):
-    LOGGER.info(cloneob.cmd)
-    if shell:
-        proc = await create_subprocess_shell(cloneob.cmd, stdout=PIPE, stderr=PIPE)
-    else:
-        proc = await create_subprocess_exec(*cloneob.cmd, stdout=PIPE, stderr=PIPE)
+
+async def pro_status(proc, cloneob):
     while True:
         try:
             async for line in proc.stderr:
+                    if cloneob.pro_id not in processes:
+                        break
                     line = line.decode('utf-8').strip()
                     if len(str(line)):
                         print(line)
@@ -76,23 +78,31 @@ async def cmd_exec_status(cloneob, shell=False):
                 continue
         else:
                 break
-    await proc.wait()
-    cloneob.set_returncode(proc.returncode)
-    cloneob.completed()
+    if cloneob.pro_id in processes:
+        await proc.wait()
+        cloneob.set_returncode(proc.returncode)
+        cloneob.completed()
+    else:
+        LOGGER.info("Process Cancelled")
     return
 
 
 async def update_status(cloneob, msg):
     pre_msg = ''
     while True:
+            if cloneob.pro_id not in processes:
+                    break
+            if cloneob.complete:
+                    LOGGER.info("Clone Completed")
+                    break
             if cloneob.currentmsg and pre_msg!=cloneob.currentmsg:
                     await editMessage(msg, cloneob.currentmsg)
                     pre_msg = cloneob.currentmsg
-            if cloneob.complete:
-                LOGGER.info("Clone Completed")
-                break
             await sleep(int(config_dict['STATUS_UPDATE_INTERVAL']))
-    if cloneob.returncode==0:
+    
+    if cloneob.pro_id not in processes:
+            await editMessage(msg, f"Copying Cancelled")
+    elif cloneob.returncode==0:
             await editMessage(msg, f"Copying Completed")
     else:
             await editMessage(msg, f"Copying Failed\nReturn Code: {cloneob.returncode}\n\n{cloneob.log}")
@@ -111,6 +121,14 @@ async def clone(_, message):
         name  = input_list[2]
     except:
         name = ""
+    if link=="cancel":
+        if name in processes:
+            processes.remove(name)
+            await sendMessage(message, 'Process Cancelled')
+            return
+        else:
+            await sendMessage(message, 'Process ID Not Found')
+            return
     if is_share_link(link):
         try:
             link = await sync_to_async(direct_link_generator, link)
@@ -139,10 +157,16 @@ async def clone(_, message):
                 "--transfers=16",
                 "--checkers=20",
             ]
-        msg = await sendMessage(message, "Cloning, Please Wait...")
-        cloneob = CloneHelper(cmd)
-        bot_loop.create_task(update_status(cloneob, msg))
-        await cmd_exec_status(cloneob)
+        LOGGER.info(cmd)
+        pro_id = str(''.join(choices(ascii_lowercase + digits, k=7)))
+        processes.add(pro_id)
+        proc = await create_subprocess_exec(*cmd, stdout=PIPE, stderr=PIPE)
+        cloneob = CloneHelper(proc, pro_id)
+        msg = await sendMessage(message, f"Initializing Cloning, Please Wait...\n\nCancel: <code>/clone cancel {pro_id}</code>")
+        bot_loop.create_task(pro_status(proc, cloneob))
+        await update_status(cloneob, msg)
+        if cloneob.pro_id in processes:
+            processes.remove(pro_id)
     return
 
 
